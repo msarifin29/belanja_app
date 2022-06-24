845bf7825019ff9b954d2f3b54ccdfd649b14c43
M S Arifin <ms.arifin29@gmail.com>
2022-06-24 21:37:22 +0700

    adding widget product_grid

---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,30 @@
+import 'package:belanja_app/widgets/product_item.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/products.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final providerData = Provider.of<Products>(context);
+    final product = providerData.items;
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: 3 / 2,
+            mainAxisExtent: 150),
+        padding: const EdgeInsets.all(10),
+        itemCount: product.length,
+        itemBuilder: (ctx, i) => ProductItem(
+            id: product[i].id,
+            title: product[i].title,
+            imageUrl: product[i].imageUrl));
+  }
+}
