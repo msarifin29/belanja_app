38b5803398518a22749b77e0a1aa75a3cf21c469
M S Arifin <ms.arifin29@gmail.com>
2022-06-28 21:32:29 +0700

    adding detail product

--- lib/screen/product_detail_screen.dart
+++ lib/screen/product_detail_screen.dart
@@ -16,40 +16,40 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
-      // body: SingleChildScrollView(
-      //     child: Column(
-      //   children: [
-      //     Container(
-      //       height: 300,
-      //       width: double.infinity,
-      //       child: Image.network(
-      //         loadedProduct.imageUrl,
-      //         fit: BoxFit.cover,
-      //       ),
-      //     ),
-      //     const SizedBox(
-      //       height: 20,
-      //     ),
-      //     Text(
-      //       '\$${loadedProduct.price}',
-      //       style: const TextStyle(fontSize: 20, color: Colors.grey),
-      //     ),
-      //     const SizedBox(
-      //       height: 10,
-      //     ),
-      //     Container(
-      //       width: double.infinity,
-      //       padding: const EdgeInsets.symmetric(
-      //         horizontal: 10,
-      //       ),
-      //       child: Text(
-      //         loadedProduct.description,
-      //         softWrap: true,
-      //         textAlign: TextAlign.center,
-      //       ),
-      //     )
-      //   ],
-      // )),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              loadedProduct.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            '\$${loadedProduct.price}',
+            style: const TextStyle(fontSize: 20, color: Colors.grey),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            child: Text(
+              loadedProduct.description,
+              softWrap: true,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      )),
     );
   }
 }
