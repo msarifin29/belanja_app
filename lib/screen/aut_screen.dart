1a95194adc14609ce254f78992032bf278cd9dc6
M S Arifin <ms.arifin29@gmail.com>
2022-07-16 18:42:51 +0700

    Animation & Transition widgets

--- lib/screen/aut_screen.dart
+++ lib/screen/aut_screen.dart
@@ -49,7 +49,7 @@ class AuthScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(
-                  height: 40,
+                  height: 20,
                 ),
                 Flexible(
                   child: const AuthCard(),
@@ -71,7 +71,8 @@ class AuthCard extends StatefulWidget {
   State<AuthCard> createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   // ignore: prefer_final_fields
@@ -79,6 +80,32 @@ class _AuthCardState extends State<AuthCard> {
 
   var _isLoading = false;
   final passwordController = TextEditingController();
+  // controller animation
+  AnimationController? _controller;
+  Animation<Offset>? _slideAnimation;
+  Animation<double>? _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    _slideAnimation =
+        Tween<Offset>(begin: const Offset(0, -1.5), end: const Offset(0, 0))
+            .animate(
+      CurvedAnimation(
+          parent: _controller!, curve: Curves.fastLinearToSlowEaseIn),
+    );
+    _opacityAnimation = Tween(begin: 0.0, end: 1.0)
+        .animate(CurvedAnimation(parent: _controller!, curve: Curves.easeIn));
+    // _controller!.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller!.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -145,10 +172,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _controller!.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _controller!.reverse();
     }
   }
 
@@ -157,93 +186,109 @@ class _AuthCardState extends State<AuthCard> {
     final device = MediaQuery.of(context).size;
 
     return Card(
-      elevation: 8.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Container(
-        height: _authMode == AuthMode.Signup ? 350 : 300,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 350 : 300),
-        width: device.width * 0.75,
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(labelText: 'E-mail'),
-                    validator: (value) {
-                      if (value!.isEmpty || !value.contains('@')) {
-                        return 'Email salah';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _authData['email'] = value;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                    controller: passwordController,
-                    validator: (value) {
-                      if (value!.isEmpty || value.length < 5) {
-                        return 'Password minimal terdiri dari 5 karakter!';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _authData['password'] = value;
-                    },
-                  ),
-                  if (_authMode == AuthMode.Signup)
+        elevation: 8.0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeIn,
+          height: _authMode == AuthMode.Signup ? 320 : 260,
+          constraints: BoxConstraints(
+            minHeight: _authMode == AuthMode.Signup ? 320 : 260,
+          ),
+          width: device.width * 0.75,
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
                     TextFormField(
-                        decoration: const InputDecoration(
-                            labelText: 'Confirm password'),
-                        obscureText: true,
-                        enabled: _authMode == AuthMode.Signup,
-                        validator: _authMode == AuthMode.Signup
-                            ? (value) {
-                                if (value != passwordController.text) {
-                                  return 'Kata sandi tidak cocok!';
-                                }
-                                return null;
-                              }
-                            : null),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  if (_isLoading)
-                    const CircularProgressIndicator()
-                  else
-                    ElevatedButton(
-                      onPressed: _submit,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(labelText: 'E-mail'),
+                      validator: (value) {
+                        if (value!.isEmpty || !value.contains('@')) {
+                          return 'Email salah';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _authData['email'] = value;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                      controller: passwordController,
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 5) {
+                          return 'Password minimal terdiri dari 5 karakter!';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _authData['password'] = value;
+                      },
+                    ),
+                    // if (_authMode == AuthMode.Signup)
+                    AnimatedContainer(
+                      constraints: BoxConstraints(
+                        minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                        maxHeight: _authMode == AuthMode.Signup ? 160 : 0,
+                      ),
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeIn,
+                      child: FadeTransition(
+                        opacity: _opacityAnimation!,
+                        child: SlideTransition(
+                          position: _slideAnimation!,
+                          child: TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Confirm password'),
+                              obscureText: true,
+                              enabled: _authMode == AuthMode.Signup,
+                              validator: _authMode == AuthMode.Signup
+                                  ? (value) {
+                                      if (value != passwordController.text) {
+                                        return 'Kata sandi tidak cocok!';
+                                      }
+                                      return null;
+                                    }
+                                  : null),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    if (_isLoading)
+                      const CircularProgressIndicator()
+                    else
+                      ElevatedButton(
+                        onPressed: _submit,
+                        child: Text(
+                            _authMode == AuthMode.Login ? 'LOGIN' : 'SIGNUP'),
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 30.0),
+                            primary: Theme.of(context).primaryColor),
+                      ),
+                    TextButton(
+                      onPressed: _switchAuth,
                       child: Text(
-                          _authMode == AuthMode.Login ? 'LOGIN' : 'SIGNUP'),
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
+                        "${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} ",
+                      ),
+                      style: TextButton.styleFrom(
                           padding: const EdgeInsets.symmetric(
-                              vertical: 8.0, horizontal: 30.0),
+                              horizontal: 30, vertical: 4),
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                           primary: Theme.of(context).primaryColor),
-                    ),
-                  TextButton(
-                    onPressed: _switchAuth,
-                    child: Text(
-                      "${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} ",
-                    ),
-                    style: TextButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 30, vertical: 4),
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        primary: Theme.of(context).primaryColor),
-                  )
-                ],
-              ),
-            )),
-      ),
-    );
+                    )
+                  ],
+                ),
+              )),
+        ));
   }
 }
